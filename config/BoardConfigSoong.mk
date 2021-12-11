166374a051addc89d032978d6a6624262af924db
Sagarrokade006 <Sagarrokade006@gmail.com>
2021-12-11 03:39:42 +0000

    Hmm

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -40,7 +40,8 @@ SOONG_CONFIG_octaviGlobalVars += \
     target_ld_shim_libs \
     target_process_sdk_version_override \
     target_surfaceflinger_udfps_lib \
-    uses_camera_parameter_lib
+    uses_camera_parameter_lib \
+    camera_needs_client_info_lib \
 
 SOONG_CONFIG_NAMESPACES += octaviNvidiaVars
 SOONG_CONFIG_octaviNvidiaVars += \
@@ -89,6 +90,7 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_octaviGlobalVars_additional_gralloc_10_usage_bits := $(TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS)
+SOONG_CONFIG_octaviGlobalVars_camera_needs_client_info_lib := $(TARGET_CAMERA_NEEDS_CLIENT_INFO_LIB)
 SOONG_CONFIG_octaviGlobalVars_bootloader_message_offset := $(BOOTLOADER_MESSAGE_OFFSET)
 SOONG_CONFIG_octaviGlobalVars_target_init_vendor_lib := $(TARGET_INIT_VENDOR_LIB)
 SOONG_CONFIG_octaviGlobalVars_target_ld_shim_libs := $(subst $(space),:,$(TARGET_LD_SHIM_LIBS))
